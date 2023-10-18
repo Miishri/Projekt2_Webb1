b1a201196585f8762de9102d77ec50ab9cb02490
Muhammad Shamaeem <muhammad.shamaeem@elev.ga.ntig.se>
2023-10-18 09:11:45 +0200

    complete draft design

--- script.js
+++ script.js
@@ -1,20 +1,25 @@
-import {fetchCpu, fetchDisplay, fetchGpu, fetchRam, getAllComponents} from "./model/ComponentFactory.js";
+import {
+    fetchCpu,
+    fetchDisplay,
+    fetchGpu,
+    fetchMotherboard,
+    fetchRam,
+    fetchSsd,
+    getAllComponents
+} from "./model/ComponentFactory.js";
 import {createProduct, createRecommendationProducts} from "./model/Product.js";
 
 const discountSlider = document.getElementById("slider");
 const dots = document.querySelectorAll(".dot")
-
 let imageIndex = 0
 
 setInterval(updateSlider, 2000)
-
 function updateSlider() {
     imageIndex = (imageIndex + 1) % dots.length;
     discountSlider.scrollLeft = imageIndex * discountSlider.clientWidth;
     
     updateDot();
 }
-
 function updateDot() {
     dots.forEach((dot) => {
         dot.classList.remove("current-dot")
@@ -32,15 +37,56 @@ async function populateRecommendedProducts() {
     createRecommendationProducts(singleRam[5])
     createRecommendationProducts(singleMonitor[0])
 }
-populateRecommendedProducts()
+await populateRecommendedProducts()
 
 const gpu = document.querySelector(".gpu")
+gpu.addEventListener('click', async () => {
+    addActiveCategory(gpu)
+    displayPagedProducts(await fetchGpu())
+})  
 const cpu = document.querySelector(".cpu")
+cpu.addEventListener('click', async () => {
+    addActiveCategory(cpu)
+    displayPagedProducts(await fetchCpu())
+})
 const ssd = document.querySelector(".ssd")
+ssd.addEventListener('click', async () => {
+    addActiveCategory(ssd)
+    displayPagedProducts(await fetchSsd())
+})
 const monitor = document.querySelector(".monitor")
+monitor.addEventListener('click', async () => {
+    addActiveCategory(monitor)
+    displayPagedProducts(await fetchDisplay())
+})
 const ram = document.querySelector(".ram")
+ram.addEventListener('click', async () => {
+    addActiveCategory(ram)
+    displayPagedProducts(await fetchRam())
+})
 const motherboard = document.querySelector(".motherboard")
+motherboard.addEventListener('click', async () => {
+    addActiveCategory(motherboard)
+    displayPagedProducts(await fetchMotherboard())
+})
 
+function addActiveCategory(element) {
+    hideAllPages()
+    removeAllActiveCategories()
+    element.classList.add("active-category")
+}
+
+function hideAllPages() {
+    document.querySelectorAll(".products-paging").forEach((page) => {
+        page.classList.add("hide-page")
+    })
+}
+
+function removeAllActiveCategories() {
+    document.querySelectorAll(".categories").forEach((cat) => {
+        cat.classList.remove("active-category");
+    });
+}
 
 
 let loadedProducts = false;
@@ -75,20 +121,11 @@ function loadPageEvent(i) {
         currentPage = i;
     });
 }
-
-const next = document.querySelector(".next")
-const back = document.querySelector(".back")
-
-next.addEventListener('click', () => {
-    if (currentPage <= 6 && currentPage + 1 !== 7) {
-        showPage(++currentPage)
-    }
-})
-
-back.addEventListener('click', () => {
+document.querySelector(".next").addEventListener('click', () => endOfNextPage())
+function endOfNextPage(){if (currentPage <= 6 && currentPage + 1 !== 7) showPage(++currentPage)}
+document.querySelector(".back").addEventListener('click', () => endOfBackPage())
+function endOfBackPage() {
     if (currentPage <= 6 && currentPage - 1 !== 0) {
         showPage(--currentPage)
     }
-})
-
-
+}
