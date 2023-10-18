9954cd198080f606369efe2d395ed6bb145a5c3e
Muhammad Shamaeem <muhammad.shamaeem@elev.ga.ntig.se>
2023-10-18 21:18:54 +0200

    reset code

--- script.js
+++ script.js
@@ -3,121 +3,124 @@ import {
     fetchDisplay,
     fetchGpu,
     fetchMotherboard,
-    fetchRam,
-    fetchSsd,
+    fetchRam, fetchSsd,
     getAllComponents
 } from "./model/ComponentFactory.js";
-
-import {
-    createProduct,
-    createRecommendationProducts
-} from "./model/Product.js";
+import {createProduct, createRecommendationProducts} from "./model/Product.js";
 
 const discountSlider = document.getElementById("slider");
-const dots = document.querySelectorAll(".dot");
+const dots = document.querySelectorAll(".dot")
 const products = document.querySelector(".products");
-const categories = document.querySelectorAll(".category");
+const categories = document.querySelectorAll(".category")
 
-let imageIndex = 0;
-let currentPage = 1;
-const itemsPerPage = 12;
+let imageIndex = 0
 let productsLoaded = false;
-let components = [];
+const itemsPerPage = 12;
+const components = await getAllComponents();
+let currentPage = 1;
 
-async function fetchData() {
-    components = await getAllComponents();
-    createRecommendationProducts(components[3]);
-    createRecommendationProducts(components[14]);
-    createRecommendationProducts(components[34]);
-    showPage(currentPage);
-    productsLoaded = true;
-}
-
-fetchData();
-
+setInterval(updateSlider, 2000)
 function updateSlider() {
     imageIndex = (imageIndex + 1) % dots.length;
     discountSlider.scrollLeft = imageIndex * discountSlider.clientWidth;
+
     updateDot();
 }
-
 function updateDot() {
-    dots.forEach(dot => dot.classList.remove("current-dot"));
-    dots[imageIndex].classList.add("current-dot");
+    dots.forEach((dot) => {
+        dot.classList.remove("current-dot")
+    })
+    dots[imageIndex].classList.add("current-dot")
 }
+updateDot();
 
-setInterval(updateSlider, 2000);
+createRecommendationProducts(components[3])
+createRecommendationProducts(components[14])
+createRecommendationProducts(components[34])
 
-function loadComponent(component) {
-    hidePaging();
-    removeActiveCategories();
-    const componentId = component.id;
-    addActiveCategory(component);
-    switch (componentId) {
-        case "gpu":
-            fetchAndDisplay(fetchGpu);
-            break;
-        case "cpu":
-            fetchAndDisplay(fetchCpu);
-            break;
-        case "motherboard":
-            fetchAndDisplay(fetchMotherboard);
-            break;
-        case "ram":
-            fetchAndDisplay(fetchRam);
-            break;
-        case "monitor":
-            fetchAndDisplay(fetchDisplay);
-            break;
-        case "ssd":
-            fetchAndDisplay(fetchSsd);
-            break;
-    }
-}
-
-function fetchAndDisplay(fetchFunction) {
-    fetchFunction().then(data => displayPagedProducts(data)).catch(error => console.error(error));
-}
-
-function addActiveCategory(element) {
-    element.classList.add("active-category");
-}
-
-function removeActiveCategories() {
-    categories.forEach(category => category.classList.remove("active-category"));
-}
-
-const disableCategories = () => categories.forEach(category => category.disabled = true);
-const enableCategories = () => categories.forEach(category => category.disabled = false);
-
-categories.forEach(category => {
+categories.forEach((category) => {
     category.addEventListener('click', () => {
         if (!category.classList.contains('active-category')) {
             disableCategories();
             loadComponent(category);
             enableCategories();
-        } else {
-            removeActiveCategories();
-            showPaging();
-            showPage(currentPage);
+        }else {
+            removeActiveCategories()
+            showPaging()
+            showPage(currentPage)
         }
+    })
+})
+const loadComponent = async function (component) {
+    hidePaging()
+    removeActiveCategories()
+    const componentId = component.id
+    addActiveCategory(component)
+    switch (componentId) {
+        case "gpu":
+            displayPagedProducts(await fetchGpu())
+            break;
+        case "cpu":
+            displayPagedProducts(await fetchCpu())
+            break;
+        case "motherboard":
+            displayPagedProducts(await fetchMotherboard())
+            break;
+        case "ram":
+            displayPagedProducts(await fetchRam())
+            break;
+        case "monitor":
+            displayPagedProducts(await fetchDisplay())
+            break;
+        case "ssd":
+            displayPagedProducts(await fetchSsd())
+            break;
+    }
+}
+
+function addActiveCategory(element) {
+    element.classList.add("active-category")
+}
+
+function removeActiveCategories() {
+    categories.forEach((category) => {
+        category.classList.remove("active-category")
+    })
+}
+
+
+const disableCategories = () => {
+    categories.forEach(category => {
+        category.disabled = true;
     });
-});
+};
+
+const enableCategories = () => {
+    categories.forEach(category => {
+        category.disabled = false;
+    });
+};
+
+if (!productsLoaded) {
+    showPage(currentPage);
+    productsLoaded = true;
+}
 
 function showPage(pageNumber) {
     const startIndex = (pageNumber - 1) * itemsPerPage;
     const endIndex = startIndex + itemsPerPage;
     displayPagedProducts(components.slice(startIndex, endIndex));
 }
-
 function displayPagedProducts(splicedComponents) {
     products.innerHTML = '';
-    splicedComponents.forEach(product => {
+    splicedComponents.forEach((product) => {
         createProduct(product);
     });
 }
-
 for (let i = 1; i <= 6; i++) {
+    loadPageEvent(i)
+}
+function loadPageEvent(i) {
     const page = document.querySelector(`.page-${i}`);
     page.addEventListener('click', () => {
         showPage(i);
@@ -125,22 +128,17 @@ for (let i = 1; i <= 6; i++) {
     });
 }
 
-document.querySelector(".next").addEventListener('click', () => {
-    if (currentPage < 6) {
-        showPage(++currentPage);
+document.querySelector(".next").addEventListener('click', () => endOfNextPage())
+function endOfNextPage(){if (currentPage <= 6 && currentPage + 1 !== 7) showPage(++currentPage)}
+document.querySelector(".back").addEventListener('click', () => endOfBackPage())
+function endOfBackPage() {
+    if (currentPage <= 6 && currentPage - 1 !== 0) {
+        showPage(--currentPage)
     }
-});
-
-document.querySelector(".back").addEventListener('click', () => {
-    if (currentPage > 1) {
-        showPage(--currentPage);
-    }
-});
-
-function hidePaging() {
-    document.getElementById("products-paginator").style.display = 'none';
 }
-
+function hidePaging() {
+    document.getElementById("products-paginator").style.display = 'none'
+}
 function showPaging() {
-    document.getElementById("products-paginator").style.display = 'flex';
+    document.getElementById("products-paginator").style.display = 'flex'
 }
