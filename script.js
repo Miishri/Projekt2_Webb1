9e299f6b11542020ccddafafd0ebf88b91c2196e
Muhammad Shamaeem <muhammad.shamaeem@elev.ga.ntig.se>
2023-10-18 20:56:53 +0200

    test-optimized

--- script.js
+++ script.js
@@ -8,137 +8,91 @@ import {
 } from "./model/ComponentFactory.js";
 import {createProduct, createRecommendationProducts} from "./model/Product.js";
 
-const discountSlider = document.getElementById("slider");
-const dots = document.querySelectorAll(".dot")
-const products = document.querySelector(".products");
-const categories = document.querySelectorAll(".category")
+const productsContainer = document.querySelector(".products");
+const paginator = document.getElementById("products-paginator");
 
-let imageIndex = 0
-let productsLoaded = false;
-const itemsPerPage = 12;
-const components = await getAllComponents();
+const categoryContainer = document.querySelector(".categories");
+categoryContainer.addEventListener('click', handleCategoryClick);
+
 let currentPage = 1;
+const itemsPerPage = 12;
+let productsLoaded = false;
+let components = [];
 
-setInterval(updateSlider, 2000)
-function updateSlider() {
-    imageIndex = (imageIndex + 1) % dots.length;
-    discountSlider.scrollLeft = imageIndex * discountSlider.clientWidth;
-    
-    updateDot();
-}
-function updateDot() {
-    dots.forEach((dot) => {
-        dot.classList.remove("current-dot")
-    })
-    dots[imageIndex].classList.add("current-dot")
-}
-updateDot();
-
-createRecommendationProducts(components[3])
-createRecommendationProducts(components[14])
-createRecommendationProducts(components[34])
-
-categories.forEach((category) => {
-    category.addEventListener('click', () => {
-        if (!category.classList.contains('active-category')) {
-            disableCategories();
-            loadComponent(category);
-            enableCategories();
-        }else {
-            removeActiveCategories()
-            showPaging()
-            showPage(currentPage)
-        }
-    })
-})
-const loadComponent = async function (component) {
-    hidePaging()
-    removeActiveCategories()
-    const componentId = component.id
-    addActiveCategory(component)
-    switch (componentId) {
-        case "gpu":
-            displayPagedProducts(await fetchGpu())
-            break;
-        case "cpu":
-            displayPagedProducts(await fetchCpu())
-            break;
-        case "motherboard":
-            displayPagedProducts(await fetchMotherboard())
-            break;
-        case "ram":
-            displayPagedProducts(await fetchRam())
-            break;
-        case "monitor":
-            displayPagedProducts(await fetchDisplay())
-            break;
-        case "ssd":
-            displayPagedProducts(await fetchSsd())
-            break;
-    }
-} 
-
-function addActiveCategory(element) {
-    element.classList.add("active-category")
-}
-
-function removeActiveCategories() {
-    categories.forEach((category) => {
-        category.classList.remove("active-category")
-    })
-}
-
-
-const disableCategories = () => {
-    categories.forEach(category => {
-        category.disabled = true;
-    });
-};
-
-const enableCategories = () => {
-    categories.forEach(category => {
-        category.disabled = false;
-    });
-};
-
-if (!productsLoaded) {
+async function initializeComponents() {
+    components = await getAllComponents();
     showPage(currentPage);
     productsLoaded = true;
 }
 
-function showPage(pageNumber) {
-    const startIndex = (pageNumber - 1) * itemsPerPage;
-    const endIndex = startIndex + itemsPerPage;
-    displayPagedProducts(components.slice(startIndex, endIndex));
+function handleCategoryClick(event) {
+    const category = event.target;
+    if (category.classList.contains('category') && !category.classList.contains('active-category')) {
+        hidePaging();
+        removeActiveCategories();
+        loadComponent(category);
+        addActiveCategory(category);
+        showPaging();
+    }
 }
-function displayPagedProducts(splicedComponents) {
-    products.innerHTML = '';
-    splicedComponents.forEach((product) => {
-        createProduct(product);
+
+async function loadComponent(component) {
+    let componentData = [];
+    switch (component.id) {
+        case "gpu":
+            componentData = await fetchGpu();
+            break;
+        case "cpu":
+            componentData = await fetchCpu();
+            break;
+        case "motherboard":
+            componentData = await fetchMotherboard();
+            break;
+        case "ram":
+            componentData = await fetchRam();
+            break;
+        case "monitor":
+            componentData = await fetchDisplay();
+            break;
+        case "ssd":
+            componentData = await fetchSsd();
+            break;
+    }
+    displayPagedProducts(componentData);
+}
+
+function displayPagedProducts(componentData) {
+    const fragment = document.createDocumentFragment();
+    productsContainer.innerHTML = '';
+    componentData.slice((currentPage - 1) * itemsPerPage, currentPage * itemsPerPage).forEach((product) => {
+        const productElement = createProduct(product);
+        fragment.appendChild(productElement);
     });
+    productsContainer.appendChild(fragment);
 }
-for (let i = 1; i <= 6; i++) {
-    loadPageEvent(i)
+
+function showPage(pageNumber) {
+    currentPage = pageNumber;
+    displayPagedProducts(components);
 }
-function loadPageEvent(i) {
-    const page = document.querySelector(`.page-${i}`);
-    page.addEventListener('click', () => {
-        showPage(i);
-        currentPage = i;
+
+function addActiveCategory(element) {
+    removeActiveCategories();
+    element.classList.add("active-category");
+}
+
+function removeActiveCategories() {
+    document.querySelectorAll('.category').forEach((category) => {
+        category.classList.remove("active-category");
     });
 }
 
-document.querySelector(".next").addEventListener('click', () => endOfNextPage())
-function endOfNextPage(){if (currentPage <= 6 && currentPage + 1 !== 7) showPage(++currentPage)}
-document.querySelector(".back").addEventListener('click', () => endOfBackPage())
-function endOfBackPage() {
-    if (currentPage <= 6 && currentPage - 1 !== 0) {
-        showPage(--currentPage)
-    }
-}
 function hidePaging() {
-    document.getElementById("products-paginator").style.display = 'none'
+    paginator.style.display = 'none';
 }
+
 function showPaging() {
-    document.getElementById("products-paginator").style.display = 'flex'
+    paginator.style.display = 'flex';
 }
+
+initializeComponents();
