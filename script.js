5f905718c0d5a93ac742ac997703a4f26dac1039
Muhammad Shamaeem <muhammad.shamaeem@elev.ga.ntig.se>
2023-10-25 20:49:27 +0200

    fix slide-in

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
 import {loadProducts} from "./model/AccessoriesProducts.js";
 loadProducts()
+slideCart()
 
+
+function slideCart() {
+    const cartButton = document.querySelector(".cart-logo")
+    const shoppingCart = document.getElementById("shopping-cart")
+    cartButton.addEventListener('click', () => {
+        if (shoppingCart.style.right === "0px") {
+            shoppingCart.style.right = "-100%"
+        }else {
+            shoppingCart.style.right = "0"
+        }
+    })
+}
